920f4fe4cfe0b2bbc2d967887c36ab72b699926d
afei <815609532@qq.com>
2018-11-27 23:39:52 +0800

    add TestData.java

--- app/src/main/java/com/afei/androidipc/MainActivity.java
+++ app/src/main/java/com/afei/androidipc/MainActivity.java
@@ -1,13 +1,39 @@
+/*
+ * Copyright © 2018 by afei. All rights reserved.
+ *
+ * @file: MainActivity.java
+ * @author: afei
+ * @data: 2018年11月27日
+ */
+
 package com.afei.androidipc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mAidlBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        mAidlBtn = findViewById(R.id.aidl_btn);
+        mAidlBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.aidl_btn:
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/afei/androidipc/TestData.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright © 2018 by afei. All rights reserved.
+ *
+ * @file: TestData.java
+ * @author: afei
+ * @data: 2018年11月27日
+ */
+
+package com.afei.androidipc;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class TestData implements Parcelable {
+
+    private String mString;
+    private int mInt;
+
+    public TestData(String string, int anInt) {
+        mString = string;
+        mInt = anInt;
+    }
+
+    public String getString() {
+        return mString;
+    }
+
+    public void setString(String string) {
+        mString = string;
+    }
+
+    public int getInt() {
+        return mInt;
+    }
+
+    public void setInt(int anInt) {
+        mInt = anInt;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.mString);
+        dest.writeInt(this.mInt);
+    }
+
+    protected TestData(Parcel in) {
+        this.mString = in.readString();
+        this.mInt = in.readInt();
+    }
+
+    public static final Parcelable.Creator<TestData> CREATOR = new Parcelable.Creator<TestData>() {
+        @Override
+        public TestData createFromParcel(Parcel source) {
+            return new TestData(source);
+        }
+
+        @Override
+        public TestData[] newArray(int size) {
+            return new TestData[size];
+        }
+    };
+
+    @Override
+    public String
+    toString() {
+        return "TestData{" + "mString='" + mString + '\'' + ", mInt=" + mInt + '}';
+    }
+}
